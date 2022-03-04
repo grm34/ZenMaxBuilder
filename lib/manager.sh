4fff5708e7ca2534d2dc8bff19a9fd353069f3ff
grm34 <jerem.pardo@tutanota.com>
2022-03-04 12:31:31 +0100

    Prevent inputs logs on external opts
    Signed-off-by: grm34 <jerem.pardo@tutanota.com>

--- lib/manager.sh
+++ lib/manager.sh
@@ -131,9 +131,11 @@ $((BUILD_TIME / 60)) minutes and $((BUILD_TIME % 60)) seconds</code>"
     fi
 
     # Get user inputs and add them to logfile
-    set | grep -v "${EXCLUDE_VARS}" > buildervar
-    printf "\n### USER INPUT LOGS ###\n" >> "${LOG}"
-    diff bashvar buildervar | grep -E "^> [A-Z_]{3,18}=" >> "${LOG}"
+    if [[ -f buildervar ]]; then
+        set | grep -v "${EXCLUDE_VARS}" > buildervar
+        printf "\n### USER INPUT LOGS ###\n" >> "${LOG}"
+        diff bashvar buildervar | grep -E "^> [A-Z_]{3,18}=" >> "${LOG}"
+    fi
 
     # Remove inputs files
     FILES=(bashvar buildervar linuxver)
