cf27827a50b5d6d36b9c40768c5bd39a745cb5d0
grm34 <jerem.pardo@tutanota.com>
2022-03-06 23:38:28 +0100

    Update msg notes and bash identation
    Signed-off-by: grm34 <jerem.pardo@tutanota.com>

--- lib/updater.sh
+++ lib/updater.sh
@@ -32,7 +32,8 @@ _full_upgrade() {
     # AnyKernel
     if [[ -d ${ANYKERNEL_DIR} ]]; then
         _note "Updating AnyKernel..."
-        cd "${ANYKERNEL_DIR}" || (_error "${ANYKERNEL_DIR} not found !"; _exit)
+        cd "${ANYKERNEL_DIR}" || \
+            (_error "${ANYKERNEL_DIR} not found !"; _exit)
         git checkout "${ANYKERNEL_BRANCH}"
         git pull origin "${ANYKERNEL_BRANCH}"
         cd "${DIR}" || (_error "${DIR} not found !"; _exit)
@@ -40,7 +41,7 @@ _full_upgrade() {
 
     # Proton-Clang
     if [[ -d ${PROTON_DIR} ]]; then
-        _note "Updating Proton-Clang..."
+        _note "Updating Proton Clang..."
         cd "${PROTON_DIR}" || (_error "${PROTON_DIR} not found !"; _exit)
         git checkout "${PROTON_BRANCH}"
         git pull origin "${PROTON_BRANCH}"
@@ -49,8 +50,9 @@ _full_upgrade() {
 
     # GCC-arm64
     if [[ -d ${GCC_ARM64_DIR} ]]; then
-        _note "Updating GCC-arm64..."
-        cd "${GCC_ARM64_DIR}" || (_error "${GCC_ARM64_DIR} not found !"; _exit)
+        _note "Updating GCC ARM64..."
+        cd "${GCC_ARM64_DIR}" || \
+            (_error "${GCC_ARM64_DIR} not found !"; _exit)
         git checkout "${GCC_ARM64_BRANCH}"
         git pull origin "${GCC_ARM64_BRANCH}"
         cd "${DIR}" || (_error "${DIR} not found !"; _exit)
@@ -58,7 +60,7 @@ _full_upgrade() {
 
     # GCC-arm32
     if [[ -d ${GCC_ARM_DIR} ]]; then
-        _note "Updating GCC-arm..."
+        _note "Updating GCC ARM..."
         cd "${GCC_ARM_DIR}" || (_error "${GCC_ARM_DIR} not found !"; _exit)
         git checkout "${GCC_ARM_BRANCH}"
         git pull origin "${GCC_ARM_BRANCH}"
