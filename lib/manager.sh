03a3b43a77148da84f35a10e24164346644a166c
grm34 <jerem.pardo@tutanota.com>
2022-03-08 10:49:00 +0100

    Fix var name
    Signed-off-by: grm34 <jerem.pardo@tutanota.com>

--- lib/manager.sh
+++ lib/manager.sh
@@ -110,7 +110,7 @@ _notify_error() {
     SRC_LINE="${BASH_LINENO[$i+1]}"
     SRC_FUNC="${FUNCNAME[$i+1]}"
     SRC_FILE="${BASH_SOURCE[$i+1]##*/}"
-    _error "${BASH_COMAND} (${SRC_LINE}: ${SRC_FUNC} > ${SRC_FILE##*/})"
+    _error "${BASH_COMMAND} (${SRC_LINE}: ${SRC_FUNC} > ${SRC_FILE##*/})"
 }
 
 
