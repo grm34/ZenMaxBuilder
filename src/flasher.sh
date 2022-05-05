4a3b657c4344baea7fc0dede1154fa0944c4cf52
grm34 <jerem.pardo@tutanota.com>
2022-05-05 02:15:26 +0200

    ZMB: Updates anykernel configuration process (AK3)
    Signed-off-by: grm34 <jerem.pardo@tutanota.com>

--- src/flasher.sh
+++ src/flasher.sh
@@ -97,29 +97,18 @@ _set_ak3_conf() {
     fi
 
     # anykernel.sh
-    _check sed -i \
-        "s/kernel.string=.*/kernel.string=${TAG}-${CODENAME}/g" \
-        anykernel.sh
-    _check sed -i \
-        "s/kernel.for=.*/kernel.for=${KERNEL_VARIANT}/g" \
-        anykernel.sh
-    _check sed -i \
-        "s/kernel.compiler=.*/kernel.compiler=${COMPILER}/g" \
-        anykernel.sh
-    _check sed -i \
-        "s/kernel.made=.*/kernel.made=${BUILDER}/g" \
-        anykernel.sh
-    _check sed -i \
-        "s/kernel.version=.*/kernel.version=${LINUX_VERSION}/g" \
-        anykernel.sh
-    _check sed -i \
-        "s/message.word=.*/message.word=ZenMaxBuilder/g" \
-        anykernel.sh
-    _check sed -i \
-        "s/build.date=.*/build.date=${DATE}/g" \
-        anykernel.sh
-    _check sed -i \
-        "s/device.name1=.*/device.name1=${CODENAME}/g" \
-        anykernel.sh
+    strings=(
+        "s/kernel.string=.*/kernel.string=${TAG}-${CODENAME}/g"
+        "s/kernel.for=.*/kernel.for=${KERNEL_VARIANT}/g"
+        "s/kernel.compiler=.*/kernel.compiler=${COMPILER}/g"
+        "s/kernel.made=.*/kernel.made=${BUILDER}/g"
+        "s/kernel.version=.*/kernel.version=${LINUX_VERSION}/g"
+        "s/message.word=.*/message.word=ZenMaxBuilder/g"
+        "s/build.date=.*/build.date=${DATE}/g"
+        "s/device.name1=.*/device.name1=${CODENAME}/g")
+    for string in "${strings[@]}"
+    do
+        _check sed -i "$string" anykernel.sh
+    done
 }
 
