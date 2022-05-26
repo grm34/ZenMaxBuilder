f8a4cf28cdaced44df5f35e9e75d0dd5e75993b3
grm34 <jerem.pardo@tutanota.com>
2022-05-26 18:00:18 +0200

    Fix readelf chech
    Signed-off-by: grm34 <jerem.pardo@tutanota.com>

--- src/zmb.sh
+++ src/zmb.sh
@@ -1143,7 +1143,7 @@ _export_path_and_options() {
 _check_linker() {
   # ARG: $1 = cross compiler
   local linker
-  linker="$(_check readelf --all "$1" \
+  linker="$(readelf --all "$1" \
     | grep interpreter | awk -F ": " '{print $NF}')"
   linker="${linker/]}"
   if [[ -n $linker ]] && ! [[ -f $linker ]]; then
