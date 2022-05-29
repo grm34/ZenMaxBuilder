91c1e5e143a9314b7d62bbb4f75b6810ac2dd930
grm34 <jerem.pardo@tutanota.com>
2022-05-30 01:23:19 +0200

    Enforced secondary regex to prevent false positive
    Signed-off-by: grm34 <jerem.pardo@tutanota.com>

--- src/zmb.sh
+++ src/zmb.sh
@@ -1300,10 +1300,10 @@ _export_path_and_options() {
 # Ensure compiler is system supported
 _check_linker() {
   # ARG: $@ = toolchain check (from settings.cfg)
+  local r; r="^\s*\[\w{1,}\s\w{1,}\s\w{1,}:\s|\[*\\w{1,}:\s"
   for linker in "$@"; do
     linker="$(readelf --program-headers "$linker" \
-      | grep -m 1 -E "^\s*\[\w{1,}\s\w{1,}\s\w{1,}:\s|\[*:\s" \
-      | awk -F ": " '{print $NF}')"
+      | grep -m 1 -E "${r}" | awk -F ": " '{print $NF}')"
     linker="${linker/]}"
     if ! [[ -f $linker ]]; then
       _error warn "$MSG_WARN_LINKER ${red}${linker}$nc"
