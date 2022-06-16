8d20319d9f7065ee2965cf5787c4a966251ddeeb
darkmaster <6394023+grm34@users.noreply.github.com>
2022-06-16 18:14:08 +0200

    TESTING: adds device specs (jq) (#604)
    Signed-off-by: grm34 <jerem.pardo@tutanota.com>

--- tests/test.sh
+++ tests/test.sh
@@ -12,22 +12,14 @@ blue="$(tput bold setaf 4)"
 
 url="https://api-mobilespecs.azharimm.site/v2/search?query="
 
-_parse_json() {
-  # Usage: _parse_json "$file" $@"
-  local data args
-  [[ $1 =~ device ]] && args="-om 1" || args="-o"
-  for data in "${@/$1}"; do
-    grep "$args" '"'"$data"'":"[^"]*' "temp/$1" \
-      | grep -o '[^"]*$' > "temp/${data}.txt"
-  done
-}
-
-_map_data() {
-  # Usage: _map_data "$@"
-  local file name
-  for file in "$@"; do
-    name="${file##*/}"; name="${name/.txt}"
-    mapfile -t "$name" < "$file"
+_get_devices_specs() {
+  # Usage: _get_devices_specs "$@"
+  local key value
+  for key in "$@"; do
+    value="$(grep -o '"'"$key"'":"[^"]*' \
+      "temp/query.json" | grep -o '[^"]*$')"
+    IFS=$'\n' read -d "" -ra "$key" <<< "$value"
+    unset IFS
   done
 }
 
@@ -37,41 +29,46 @@ _print_devices() {
     "${yellow}${1}${nc} => ${green}$2 ${nc}(${blue}${3}${nc})"
 }
 
-_return_search() {
-  local device
-  # shellcheck disable=SC2154
-  for device in "${!phone_name[@]}"; do
-    _print_devices "$(( device + 1 ))" \
-      "${phone_name[device]}" "${brand[device]}"
-  done
-}
-
 _return_device_specs() {
-  # Usage: _run_device_specs "$@"
-  local spec; echo
-  for spec in "$@"; do
-    [[ -n $spec ]] && echo -e "${green}${spec^}${nc}: ${!spec}"
+  local device_specs order key spec; echo
+  declare -A device_specs=(
+    [brand]="{brand: .data.brand}"
+    [name]="{name: .data.phone_name}"
+    [date]="{date: .data.release_date}"
+    [dimension]="{dimension: .data.dimension}"
+    [os]="{os: .data.os}"
+    [storage]="{storage: .data.storage}"
+    [price]="{price: .data.specifications[] | select(.title == \"Misc\").specs[] | select(.key == \"Price\").val[0]}"
+)
+  order=(brand name date dimension os storage price)
+  for key in "${order[@]}"; do
+    spec="${device_specs[$key]}"
+    spec="$(jq -c "$spec" temp/device.json)"
+    if [[ -n $spec ]]; then
+      IFS=":" read -r spec spec <<< "$spec"; unset IFS
+      echo -e "${green}${key^}${nc}: ${spec::-1}"
+    fi
   done
 }
 
-_search_device() {
-  # Usage: _search_device "search"
+_search_devices() {
+  # Usage: _search_devices "search"
   curl -s -L "${url}${2// /%20}" -o temp/query.json
-  if grep -sqm 1 phone_name temp/query.json; then
-    _parse_json query.json "brand" "phone_name" "detail"
-    _map_data temp/*.txt && rm temp/*.txt
-    _return_search
+  if grep -sqm 1 "phone_name" temp/query.json; then
+    local device index
+    _get_devices_specs "brand" "phone_name" "detail"
+    # shellcheck disable=SC2154
+    for device in "${!phone_name[@]}"; do
+      _print_devices "$(( device + 1 ))" \
+        "${phone_name[device]}" "${brand[device]}"
+    done
     echo "Enter the phone number to check : "
     read -r device_number
-    local index; index="$(( device_number - 1 ))"
+    index="$(( device_number - 1 ))"
     # shellcheck disable=SC2154
     curl -s -L "${detail[index]}" -o temp/device.json
     if grep -sqm 1 phone_name temp/device.json; then
-      local specs; specs=("brand" "phone_name" "release_date" \
-        "dimension" "os" "storage")
-      _parse_json device.json "${specs[@]}"
-      _map_data temp/*.txt && rm temp/*.txt
-      _return_device_specs "${specs[@]}"
+      _return_device_specs
     else
       echo "ERROR: nothing found about ${phone_name[index]}"
     fi
@@ -81,5 +78,5 @@ _search_device() {
   fi
 }
 
-[[ $1 == "-s" ]] && _search_device "$@"
+[[ $1 == "-s" ]] && _search_devices "$@"
 
