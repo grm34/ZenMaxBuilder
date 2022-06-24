ac08ca2a3de878a617e3376cdc0a6ee75e5860e5
grm34 <jerem.pardo@tutanota.com>
2022-06-25 00:05:57 +0200

    Split long lines
    Signed-off-by: grm34 <jerem.pardo@tutanota.com>

--- tests/compat.sh
+++ tests/compat.sh
@@ -3,7 +3,9 @@
 set -u
 shopt -s progcomp checkwinsize
 
-required=(bash tput sed grep find wget git curl zip tar jq expect make cmake automake autoconf llvm-ar lld lldb clang gcc ld bison perl gperf gawk flex bc python3 zstd openssl)
+required=(bash tput sed grep find wget git curl zip tar jq
+  expect make cmake automake autoconf llvm-ar lld lldb clang
+  gcc ld bison perl gperf gawk flex bc python3 zstd openssl)
 
 echo "PATH -> ${PATH}"
 for x in "${required[@]}"; do
